e7f37f68aaf0f59e33f02c93812a5e199798101f
Renan Mello <mellorenan19@gmail.com>
2025-01-08 11:31:07 -0300

    create services folder and httpclient config

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
+import { Router } from '@angular/router';
+import { LoginService } from '../../services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -11,12 +13,16 @@ import { PrimaryInputComponent } from '../../components/primary-input/primary-in
     ReactiveFormsModule,
     PrimaryInputComponent
   ],
+  providers:[LoginService],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
   loginForm!: FormGroup;
-  constructor(){
+  constructor(
+    private router: Router,
+    private loginService: LoginService
+  ){
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required,Validators.minLength(6)])
@@ -24,10 +30,17 @@ export class LoginComponent {
   }
 
   submit(){
-    console.log(this.loginForm.value);
+    //console.log(this.loginForm.value);
+    this.loginService.login(this.loginForm.value.email,this.loginForm.value.password).subscribe(
+      {
+        next:() => console.log("Sucesso"),
+        error:() => console.log("Erro")
+      }
+    )
+      
   }
   navigate(){
-    console.log(this.loginForm.value);
+    this.router.navigate(["signup"]);
   }
 
 }
---
+++ src/app/services/login.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { LoginResponse } from '../types/Login-Response.types';
+import { tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(private httpClient : HttpClient) {   }
+
+  login(name: string, password:string){
+
+    return this.httpClient.post<LoginResponse>("/login", {name, password}).pipe(
+      tap((value) =>{
+        sessionStorage.setItem("auth-token", value.token)
+      }
+      )
+    )
+
+  }
+}
