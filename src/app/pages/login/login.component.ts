be2af7f48132eeb9e4e19afb9ae50204fe85797e
Renan Mello <mellorenan19@gmail.com>
2025-01-08 14:49:45 -0300

    add animations

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -4,6 +4,7 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
 import { Router } from '@angular/router';
 import { LoginService } from '../../services/login.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -21,7 +22,8 @@ export class LoginComponent {
   loginForm!: FormGroup;
   constructor(
     private router: Router,
-    private loginService: LoginService
+    private loginService: LoginService,
+    private toastService: ToastrService
   ){
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
@@ -33,8 +35,8 @@ export class LoginComponent {
     //console.log(this.loginForm.value);
     this.loginService.login(this.loginForm.value.email,this.loginForm.value.password).subscribe(
       {
-        next:() => console.log("Sucesso"),
-        error:() => console.log("Erro")
+        next:() => this.toastService.success("Login feito com sucesso"),
+        error:() => this.toastService.error("Erro ao realizar login, tente novamente!")
       }
     )
       
